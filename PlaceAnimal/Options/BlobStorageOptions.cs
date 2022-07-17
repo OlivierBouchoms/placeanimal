c0f0db21e86da65b7826ad66f988774a0caeaaea
Olivier Bouchoms <olivier_2000@live.nl>
2022-07-17 22:24:30 +0200

    Delete azure-pipelines.yml

--- PlaceAnimal/Options/BlobStorageOptions.cs
+++ PlaceAnimal/Options/BlobStorageOptions.cs
@@ -6,9 +6,8 @@ public class BlobStorageOptions
 {
     private static BlobStorageOptions _instance;
     public static BlobStorageOptions Instance => _instance ??= new BlobStorageOptions();
-    
-    public string ConnectionString => Environment.GetEnvironmentVariable("BLOB_CONNECTION") ??
-                                       "DefaultEndpointsProtocol=https;AccountName=placeanimal8bbb;AccountKey=+kDD8httQRd1Qx9ASFnRdHF5Ya3zpfkm1Uk13gOnyI95mox/Hawb3zQYrCpEeC8CtVpKTDHu8ZIV+AStQ4gTzQ==;EndpointSuffix=core.windows.net";
 
-    public string ImageContainerName => Environment.GetEnvironmentVariable("BLOB_CONTAINER") ?? "placeanimal";
+    public string ConnectionString => Environment.GetEnvironmentVariable("BLOB_CONNECTION");
+
+    public string ImageContainerName => Environment.GetEnvironmentVariable("BLOB_CONTAINER");
 }
